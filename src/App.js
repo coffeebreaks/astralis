512af2949b7134578bbbe597ae74a7bea4a676ac
Coffeebreaks <stefan@grafikcentralen.se>
2022-07-07 08:29:40 +0200

    asd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components"
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+
+
+
+const ListRow = styled.div`
+  width: 100%;
+  text-align: left;
+  display: flex;
+
+  justify-content: space-between;
+  padding: 1rem;
+  background: #697898;
+  color: black;
+  &:nth-child(odd){
+    background: #8d94aa;
+    color: white;
+
+`
+
+const ListWrapper = styled.div`
+    max-width: 400px;
+`
+
 
 function App() {
+
+  const [data, setData] = useState()
+
+
+ const  addData = () => {
+    axios.get('http://localhost:8000/addRow').then(resp => {
+      console.log(resp)
+    })
+  }
+
+  useEffect(()=> {
+
+    axios.get('http://localhost:8000/getData').then(resp => {
+    setData(resp.data);
+    console.log(data)
+    
+  });
+    },[])
+
+    function deleteRow(id){
+      console.log(id)
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      TEst
+      <button onClick={()=> addData()}> Add row</button>
+      <ListWrapper>
+        {
+          data?.map((x)=> 
+          <ListRow>
+            <p>{x.id}</p>
+            <p>{x.name}</p>
+            <p>{x.value}</p>
+            <button onClick={()=> deleteRow(x.id)}> Delete</button>
+            </ListRow>)
+        }
+      </ListWrapper>
+    
     </div>
   );
 }
